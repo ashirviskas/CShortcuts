e99773cd78cf6ce013f080614f678a5b2efea8a4
ashirviskas <minematas@gmail.com>
2017-06-16 00:39:30 +0300

    Default shortcuts file added, now it reads all the shortcuts from there. Added apld and aplb modes for Apple Airport in DHCP and Bridge modes respectively.

--- CShortcuts.py
+++ CShortcuts.py
@@ -1,15 +1,21 @@
 import pyperclip
 import time
+import json
 
-shortcuts = {}
-shortcuts["b"] = "I'm glad I could help you, have a nice day!"
-shortcuts["apl"] = "If you're using Apple AirPort, follow these instructions: \n https://support.getcujo.com/support/solutions/articles/9000092004-dhcp-mode-apple-airport "
+DEFAULT_SHORTCUTS_FILENAME = "default_shortcuts.json"
+
+
+def read_shortcuts_from_file(filename):
+    json_data=open(filename, "r").read()
+    shortcuts_ = json.loads(json_data)
+    return shortcuts_
 
 
 if __name__ == "__main__":
+    default_shortcuts = read_shortcuts_from_file(DEFAULT_SHORTCUTS_FILENAME)
     while True:
         copied = pyperclip.paste()
-        if copied in shortcuts:
-            pyperclip.copy(shortcuts[copied])
+        if copied in default_shortcuts:
+            pyperclip.copy(default_shortcuts[copied])
         time.sleep(0.05)
 
